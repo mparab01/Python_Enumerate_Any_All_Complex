83cc3f45283984f49a133f845be183de8017397c
mparab01 <infoeastindia1@gmail.com>
2016-10-05 00:26:51 -0500

    Add files via upload

---
+++ Enumerate%2C+All%2C+Any%2C+and+Complex.py
@@ -0,0 +1,69 @@
+
+# coding: utf-8
+
+# Built-In Function in Python : Enumerate
+
+# In[14]:
+
+l = ['a','v','d','s']
+print l
+
+
+# In[16]:
+
+for (count,item) in enumerate (l):
+    print item
+    print count
+
+
+# Built-In Function in Python : Any(), All(), and Complex()
+
+# In[17]:
+
+l = [True, True]
+
+
+# In[18]:
+
+all(l)
+
+
+# In[19]:
+
+l2 = [False, True]
+
+
+# In[20]:
+
+all (l2)
+
+
+# In[21]:
+
+any(l2)
+
+
+# In[22]:
+
+l3 = [False, False]
+
+
+# In[23]:
+
+any(l3)
+
+
+# In[24]:
+
+complex(2,3)
+
+
+# In[26]:
+
+complex("2+9j")
+
+
+# In[ ]:
+
+
+
